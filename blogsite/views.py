f15af53576eb610fb636cdfe142b3991a369c21b
Paul Ayling <paulayling.dev@gmail.com>
2017-02-04 16:44:12 +0000

    Linking view to render index template

--- blogsite/views.py
+++ blogsite/views.py
@@ -1,5 +1,5 @@
 """The views of the blogsite application."""
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
@@ -7,4 +7,4 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def index():
     """The index page route."""
-    return "A simple index page"
+    return render_template('index.html')
