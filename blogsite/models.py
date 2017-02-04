b97c71859a4e8c78bcfa74b9d4ce3a6485222710
Paul Ayling <paulayling.dev@gmail.com>
2017-02-04 17:21:23 +0000

    Add constructor and representation for post model

--- blogsite/models.py
+++ blogsite/models.py
@@ -9,3 +9,12 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(128))
     body = db.Column(db.String(4096))
+
+    def __init__(self, title, body):
+        """Constructor for Post."""
+        self.title = title
+        self.body = body
+
+    def __repr__(self):
+        """Representation."""
+        return '<Post %r:%r>' % self.id, self.title
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-"""Application configuration file"""
+"""Application configuration file."""
 
 # Flask SQLAlchemy
 SQLALCHEMY_DATABASE_URI = "sqllite:///foo.db"
