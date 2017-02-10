8e2d637fd15aedf5f654ab2563d9ffeabb150ea2
Paul Ayling <paulayling.dev@gmail.com>
2017-02-10 16:47:00 +0000

    Ordering posts by descending publish date order. I.e. newest first

--- blogsite/views.py
+++ blogsite/views.py
@@ -1,5 +1,6 @@
 """The views of the blogsite application."""
 from flask import Blueprint, render_template
+from sqlalchemy import desc
 from .models import Post
 
 # Creating the views blueprint
@@ -9,5 +10,5 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def index():
     """The index page route."""
-    posts = Post.query.all()
+    posts = Post.query.order_by(desc(Post.pub_date)).all()
     return render_template('index.html', posts=posts)
