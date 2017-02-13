3bf461df97f8369fc0736c94602cc9ff5bf017a4
Paul Ayling <paulayling.dev@gmail.com>
2017-02-13 20:29:19 +0000

    Add Flask-HTMLmin extension to minify html output for production

--- blogsite/__init__.py
+++ blogsite/__init__.py
@@ -1,6 +1,7 @@
 """blogsite package."""
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_htmlmin import HTMLMIN
 
 # Setting up flask application object
 app = Flask(__name__, instance_relative_config=True)
@@ -11,6 +12,9 @@ app.config.from_pyfile('config.py')
 
 # Create the database handler using the flask application
 db = SQLAlchemy(app)
+
+# Setup htmlmin extension
+HTMLMIN(app)
 
 # db object needs to exist before this
 from .views import views
