bbd65abc2859becf4016314f7ba765c79bc181e2
Paul Ayling <paulayling.dev@gmail.com>
2017-02-13 23:46:53 +0000

    Add new views for individual post and posts under a tag
    Tags now redirect to the tag/ view
    Minor changes to styling including site header being a hyperlink
    to / view

--- blogsite/views.py
+++ blogsite/views.py
@@ -1,7 +1,7 @@
 """The views of the blogsite application."""
 from flask import Blueprint, render_template
 from sqlalchemy import desc
-from .models import Post
+from .models import Post, Tag
 
 # Creating the views blueprint
 views = Blueprint('views', __name__)
@@ -12,3 +12,17 @@ def index():
     """The index page route."""
     posts = Post.query.order_by(desc(Post.pub_date)).all()
     return render_template('index.html', posts=posts)
+
+
+@views.route('/post/<int:post_id>')
+def single_post(post_id):
+    """Create a view with a single blog post on it."""
+    post = Post.query.get_or_404(post_id)
+    return render_template('index.html', posts=[post])
+
+
+@views.route('/tag/<int:tag_id>')
+def posts_with_tag(tag_id):
+    """Create a view with all posts related to a tag."""
+    tag = Tag.query.get_or_404(tag_id)
+    return render_template('index.html', posts=tag.posts)
