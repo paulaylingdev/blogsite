efc087fccc25c61d4b2b1d6c3127d976bc2f2a0f
Paul Ayling <paulayling1@hotmail.com>
2017-04-23 09:01:53 +0000

    Fix for Cloud9 IDE. Default dev address is now 0.0.0.0:8080

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 """Main run file for dev."""
 from blogsite import app
 from werkzeug.contrib.fixers import ProxyFix
+import os
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host=os.getenv('IP','0.0.0.0'),port=os.getenv('PORT', '8080'))
